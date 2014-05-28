0c18d6188165f5e453cd9d3f693bc8bdd68f1780
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-05-28 13:25:14 -0400

    Add staff directory to setup collab

--- setup_collab.sh
+++ setup_collab.sh
@@ -5,9 +5,13 @@ git clone https://github.com/cfpb/collab.git
 mv collab/collab/local_settings_template.py collab/collab/local_settings.py
 
 echo "INSTALLED_APPS += ('mystery', )" >> collab/collab/local_settings.py
+echo "INSTALLED_APPS += ('staff_directory', )" >> collab/collab/local_settings.py
 
 pip install -r collab/requirements.txt
 pip install -r collab/requirements-test.txt
 
+git clone https://github.com/cfpb/collab-staff-directory.git
+
 cd collab
 ln -s ../mystery
+ln -s ../collab-staff-directory/staff_directory
