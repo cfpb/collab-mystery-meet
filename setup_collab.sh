9ef83909d7cc4be3f2022799b0548c60bc07af76
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-05-29 14:00:22 -0400

    Add form builder

--- setup_collab.sh
+++ setup_collab.sh
@@ -6,12 +6,15 @@ mv collab/collab/local_settings_template.py collab/collab/local_settings.py
 
 echo "INSTALLED_APPS += ('mystery', )" >> collab/collab/local_settings.py
 echo "INSTALLED_APPS += ('staff_directory', )" >> collab/collab/local_settings.py
+echo "INSTALLED_APPS += ('form_builder', )" >> collab/collab/local_settings.py
 
 pip install -r collab/requirements.txt
 pip install -r collab/requirements-test.txt
 
 git clone https://github.com/cfpb/collab-staff-directory.git
+git clone https://github.com/cfpb/collab-form-builder.git
 
 cd collab
 ln -s ../mystery
 ln -s ../collab-staff-directory/staff_directory
+ln -s ../collab-form-builder/src/form_builder
